377343d1b1ec69e99bc8a921758f7fe58e8a9264
jiwenxin90 <86370953+jiwenxin90@users.noreply.github.com>
2021-06-30 09:29:43 -0700

    Create handler.js

---
+++ handler.js
@@ -0,0 +1,24 @@
+exports.list_ids = (event, context, callback) => {
+    console.log("list ids");
+    return 1;
+};
+
+exports.create_ids = (event, context, callback) => {
+    console.log("create ids");
+    return 2;
+};
+
+exports.get_ids = (event, context, callback) => {
+    console.log("get ids");
+    return 3;
+};
+
+exports.delete_ids = (event, context, callback) => {
+    console.log("delete ids");
+    return 4;
+};
+
+exports.update_ids = (event, context, callback) => {
+    console.log("update ids");
+    return 5;
+};
