c072b84f3bda8039a7dcd1e3a4f876ffa42a1644
jiwenxin90 <86370953+jiwenxin90@users.noreply.github.com>
2021-06-30 16:06:35 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,24 +1,74 @@
 exports.list_ids = (event, context, callback) => {
     console.log("list ids");
-    return 1;
+    let response = {
+        statusCode: 200,
+        headers: {
+            "x-custom-header" : "my custom header value"
+        },
+        body: JSON.stringify({
+            value: 1
+        })
+    };
+    console.log("response: " + JSON.stringify(response))
+    return response;
 };
 
 exports.create_ids = (event, context, callback) => {
     console.log("create ids");
-    return 2;
+    let response = {
+        statusCode: 200,
+        headers: {
+            "x-custom-header" : "my custom header value"
+        },
+        body: JSON.stringify({
+            value: 2
+        })
+    };
+    console.log("response: " + JSON.stringify(response))
+    return response;
 };
 
 exports.get_ids = (event, context, callback) => {
     console.log("get ids");
-    return 3;
+    let response = {
+        statusCode: 200,
+        headers: {
+            "x-custom-header" : "my custom header value"
+        },
+        body: JSON.stringify({
+            value: 3
+        })
+    };
+    console.log("response: " + JSON.stringify(response))
+    return response;
 };
 
 exports.delete_ids = (event, context, callback) => {
     console.log("delete ids");
-    return 4;
+    let response = {
+        statusCode: 200,
+        headers: {
+            "x-custom-header" : "my custom header value"
+        },
+        body: JSON.stringify({
+            value: 4
+        })
+    };
+    console.log("response: " + JSON.stringify(response))
+    return response;
 };
 
 exports.update_ids = (event, context, callback) => {
     console.log("update ids");
-    return 5;
+    let response = {
+        statusCode: 200,
+        headers: {
+            "x-custom-header" : "my custom header value"
+        },
+        body: JSON.stringify({
+            value: 5
+        })
+    };
+    console.log("response: " + JSON.stringify(response))
+    return response;
 };
